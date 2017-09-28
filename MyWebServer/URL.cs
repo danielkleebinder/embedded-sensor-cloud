9b4c17191c286a9bd7fec841cbd1b914c22ff816
Daniel Kleebinder <daniel.kleebinder@gmx.net>
2017-09-28 13:52:30 +0200

    All exercise 1 unit tests successful

--- MyWebServer/URL.cs
+++ MyWebServer/URL.cs
@@ -4,57 +4,78 @@ using System.Linq;
 using System.Text;
 using BIF.SWE1.Interfaces;
 
-namespace MyWebServer
-{
-    public class Url : IUrl
-    {
-        public Url()
-        {
+namespace MyWebServer {
+    public class Url : IUrl {
+        private string raw;
+        private string path;
+        private IDictionary<string, string> parameters;
 
+        public Url() : this(null) { }
+
+        public Url(string raw) {
+            compileURL(raw);
         }
 
-        public Url(string raw)
-        {
+        private void compileURL(string raw) {
+            this.raw = raw;
+            parameters = new Dictionary<string, string>();
 
+            if (raw == null) {
+                return;
+            }
+
+            // Split path and parameters
+            string[] splits = raw.Split('?');
+
+            // Set path
+            if (splits.Length > 0) {
+                path = splits[0];
+            } else {
+                path = raw;
+            }
+
+            // Add parameters to parameter dictionary
+            if (splits.Length > 1) {
+                string[] parameterList = splits[1].Split('&');
+                string[] result;
+                foreach (string param in parameterList) {
+                    result = param.Split('=');
+                    if (result.Length > 1) {
+                        parameters.Add(result[0], result[1]);
+                    }
+                }
+            }
         }
 
-        public IDictionary<string, string> Parameter
-        {
+        public IDictionary<string, string> Parameter {
+            get { return parameters; }
+        }
+
+        public int ParameterCount {
+            get { return parameters.Count; }
+        }
+
+        public string Path {
+            get { return path; }
+        }
+
+        public string RawUrl {
+            get { return raw; }
+        }
+
+        public string Extension {
             get { throw new NotImplementedException(); }
         }
 
-        public int ParameterCount
-        {
+        public string FileName {
             get { throw new NotImplementedException(); }
         }
 
-        public string Path
-        {
+        public string Fragment {
             get { throw new NotImplementedException(); }
         }
 
-        public string RawUrl
-        {
-            get { throw new NotImplementedException(); }
-        }
-
-        public string Extension
-        {
-            get { throw new NotImplementedException(); }
-        }
-
-        public string FileName
-        {
-            get { throw new NotImplementedException(); }
-        }
-
-        public string Fragment
-        {
-            get { throw new NotImplementedException(); }
-        }
-
-        public string[] Segments
-        {
+        public string[] Segments {
             get { throw new NotImplementedException(); }
         }
     }
